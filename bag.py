bf99ae07f2611b9d9c5f8c6aa49550c73e777983
Christina Ford <fordc@uchicago.edu>
2018-12-19 00:24:05 -0600

    oh no

--- bag.py
+++ bag.py
@@ -3,15 +3,17 @@ import time
 
 from pushbullet import Pushbullet
 
-#set constants
+########################## set constants ##########################
 BUTTON_INPUT = 11
 FSR_INPUT = 16
 
 BUTTON_LED = 32
 FSR_LED = 36
-#end constants
+########################## end constants ##########################
 
-#setup code
+
+
+##########################   setup code  ##########################
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(BUTTON_INPUT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -20,10 +22,14 @@ GPIO.setup(BUTTON_LED, GPIO.OUT)
 GPIO.setup(FSR_LED, GPIO.OUT)
 
 pb= Pushbullet('o.jq7HoVKz2DCWJ1Vxem7E9ZvRRVEKz27T')
-#end setup
+##########################   end setup   ##########################
 
-#button
+# code logic
+sos_count = 0
+
+# loop to continuously check for detection
 while True:
+    # check input for BUTTON; check if sos req is valid
     input_state = GPIO.input(BUTTON_INPUT)
     turn_off(BUTTON_LED)
     if input_state == False:
@@ -31,8 +37,14 @@ while True:
         print('Button Pressed')
         time.sleep(0.2)
         turn_off(BUTTON_LED)
-        pb.push_note("SOS", "You requested help")
-    
+        sos_count += 1
+
+    # button has been pressed 3 times; push alert to phone
+    if (sos_count == 3):
+        pb.push_note("SOS alert", "Lat: 24123 Long: 21312 Jason needs help!")
+        sos_count = 0
+
+    # check if FSR req is valid
     fsr_state = GPIO.input(FSR_INPUT)
     turn_off(FSR_LED)
     if (fsr_state == False):
@@ -40,9 +52,10 @@ while True:
         print("fsr activated")
         time.sleep(0.2)
         turn_off(FSR_LED)
-        push = pb.push_note("Dehydration", "You need water")
+        push = pb.push_note("Low water alert", "You need to refill more water!")
                 
 
+# functions to make it easier to turn LEDs on and off
 def turn_on(port_num):
     GPIO.output(port_num, GPIO.HIGH)
     

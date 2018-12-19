1d7ec079e109267b684ab895d9e18a7f1bc5949e
Christina Ford <fordc@uchicago.edu>
2018-12-18 19:46:35 -0600

    led test code

---
+++ led_test.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+red = 32
+green = 36
+
+GPIO.setup(red,GPIO.OUT)
+GPIO.setup(green,GPIO.OUT)
+
+print "LED on"
+GPIO.output(red,GPIO.HIGH)
+GPIO.output(green,GPIO.HIGH)
+
+time.sleep(1)
+
+print "LED off"
+GPIO.output(red,GPIO.LOW)
+GPIO.output(green,GPIO.LOW)
